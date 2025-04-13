64f9a9ad0869710750eadb2dce466c501bf0937c
austins_macbooc <bomar289@gmail.com>
2025-04-13 10:40:31 -0700

    do a barrel roll intro

--- EuclideanSequencer.ino
+++ EuclideanSequencer.ino
@@ -436,12 +436,6 @@ void setup() {
 
 
 void loop() {
-//   if(BLEMidiServer.isConnected()) {             // If we've got a connection, we send an A4 during one second, at full velocity (127)
-//       BLEMidiServer.noteOn(0, 69, 127);
-//       delay(1000);
-//       BLEMidiServer.noteOff(0, 69, 127);        // Then we stop the note and make a delay of one second before returning to the beginning of the loop
-//       delay(1000);
-//   }
   updateButtons(buttons, NUM_BUTTONS);
   rotary_loop();
   keep_time();
--- led_wheel.cpp
+++ led_wheel.cpp
@@ -15,10 +15,20 @@ CRGB chan_colors[MAX_MAX_CHANNEL] = {
     CRGB::Peru
 };
 
+// private function declarations
+void do_a_barrel_roll();
+void reset_leds();
+
+
+// public function definitions
+
 void init_leds(){
     FastLED.addLeds<NEOPIXEL, LEDS_DATA_OUT>(rgbleds, NUM_LEDS);  // GRB ordering is assumed
-    FastLED.setBrightness(20); 
+    FastLED.setBrightness(10);
+    reset_leds();
+    do_a_barrel_roll();
 }
+
 
 void leds_show_playback(bool (*beatsarray)[MAX_BEATS], int chan, int beatnum){
     // light up selected beats, turn off unselected
@@ -33,4 +43,26 @@ void leds_show_playback(bool (*beatsarray)[MAX_BEATS], int chan, int beatnum){
     if (beatnum >= 0 && beatnum < MAX_BEATS)
         rgbleds[beatnum] = CRGB::White;
     FastLED.show();
+}
+
+// private function definitions
+
+void reset_leds(){
+    for (int i=0; i<NUM_LEDS; i++){
+        rgbleds[i] = CRGB::Black;
+    }
+}
+
+
+void do_a_barrel_roll(){
+    int prev_led = 0;
+    for (int j=0; j<3; j++)
+        for (int i=0; i<NUM_LEDS; i++){
+            rgbleds[i] = chan_colors(j);
+            if (i == 0) prev_led = NUM_LEDS-1;
+            rgbleds[prev_led] = CRGB::Black;
+            delay(80);
+        }
+
+    reset_leds();
 }
